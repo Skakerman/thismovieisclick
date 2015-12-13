90194094ffd9af3b5fd25f098b07af4653419e8f
Skakerman <scott.r.akerman@gmail.com>
2015-12-13 18:08:47 -0500

    Add hamburger toggle

--- app/app.js
+++ app/app.js
@@ -7,11 +7,67 @@ angular.module('clickApp', [
   'clickApp.clickHome',
   'clickApp.clickQuiz',
   'clickApp.clickFacts',
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/clickHome'});
-}]);
+  'ngHamburger'
+])
 
+.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider.otherwise({
+    redirectTo: '/clickHome'
+  });
+}])
 
-//angular.module('clickApp', ['ngMaterial']);
- 
+.controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+    $scope.tgState = false;
+    $scope.toggleLeft = buildDelayedToggler('left');
+
+    /**
+     * Supplies a function that will continue to operate until the
+     * time is up.
+     */
+    function debounce(func, wait, context) {
+      var timer;
+
+      return function debounced() {
+        var context = $scope,
+          args = Array.prototype.slice.call(arguments);
+        $timeout.cancel(timer);
+        timer = $timeout(function () {
+          timer = undefined;
+          func.apply(context, args);
+        }, wait || 10);
+      };
+    }
+
+    /**
+     * Build handler to open/close a SideNav; when animation finishes
+     * report completion in console
+     */
+    function buildDelayedToggler(navID) {
+      return debounce(function () {
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
+      }, 200);
+    }
+
+    function buildToggler(navID) {
+      return function () {
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
+      }
+    }
+  })
+  .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+    $scope.close = function () {
+      $mdSidenav('left').close()
+        .then(function () {
+          $log.debug("close LEFT is done");
+        });
+
+    };
+  });
